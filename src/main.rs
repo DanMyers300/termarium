5085ca512d5d988470270a837ad88127e53eee1a
dan <contact@danmyers.net>
2025-04-03 15:25:16 -0500

    updated comments

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 use std::{thread, time};
 
 fn main() {
-    // ANSI escape code to clear the screen and reset the cursor position
     loop {
-        // Clear the screen and reset the cursor position
+        // ANSI escape code to clear the screen
         print!("\x1B[2J\x1B[H");
 
-        // Draw the fish
         let fish = format!(
           "
               _
