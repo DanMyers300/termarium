8aba9c730f7ef0b0482d1644a8a3d2bac861a04e
dan <contact@danmyers.net>
2025-04-05 19:04:57 -0500

    updated main file

--- src/main.rs
+++ src/main.rs
@@ -11,20 +11,10 @@ mod cleanup;
 use cleanup::CleanupCursor;
 
 fn main() {
-    // Remove cursor from screen
-    print!("\x1B[?25l");
-    io::stdout().flush().unwrap();
+    setup();
 
     // Show cursor on end
     let _cleanup = CleanupCursor;
-
-    // Handle ctrl+c
-    ctrlc::set_handler(move || {
-        print!("\x1B[?25h");
-        io::stdout().flush().unwrap();
-        std::process::exit(0);
-    })
-    .expect("Error setting Ctrl+C handler");
 
     // Create the aquarium
     let mut aquarium = Aquarium::new();
@@ -37,10 +27,23 @@ fn main() {
         // Clear screen & move cursor
         print!("\x1B[2J\x1B[H");
 
-        // Render the aquarium
         aquarium.render(term_width, term_height);
 
         io::stdout().flush().unwrap();
         thread::sleep(time::Duration::from_millis(500));
     }
+}
+
+fn setup() {
+    // Remove cursor from screen
+    print!("\x1B[?25l");
+    io::stdout().flush().unwrap();
+
+    // Handle ctrl+c
+    ctrlc::set_handler(move || {
+        print!("\x1B[?25h");
+        io::stdout().flush().unwrap();
+        std::process::exit(0);
+    })
+    .expect("Error setting Ctrl+C handler");
 }
