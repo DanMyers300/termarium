906cfd3f8326c039426b22b27729bae86b64b414
Dan <contact@danmyers.net>
2026-09-12 23:15:58 -0500

    Created fish files

---
+++ src/render/draw/fish/fish.cpp
@@ -0,0 +1,7 @@
+#include "fish.hpp"
+#include "render.hpp"
+#include <vector>
+
+void fish(std::vector<Cell>& buf, int rows, int cols) {
+
+}
---
+++ src/render/draw/fish/fish.hpp
@@ -0,0 +1,9 @@
+#ifndef FISH_HPP
+#define FISH_HPP
+
+#include "render.hpp"
+#include <vector>
+
+void fish(std::vector<Cell>& buf, int rows, int cols);
+
+#endif // FISH_HPP
